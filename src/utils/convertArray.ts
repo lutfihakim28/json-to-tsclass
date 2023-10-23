5d4240d8006041d19c0abe27c7d1d9a8e5bc91ba
Nusatech <lutfihakim288@gmail.com>
2023-10-23 09:05:55 +0700

    fix tupple checker

--- src/utils/convertArray.ts
+++ src/utils/convertArray.ts
@@ -4,7 +4,7 @@ export function convertArray(key: string, value: unknown): string {
     if (index === 0) return key
     return key.charAt(0).toUpperCase() + key.slice(1)
   }).join('')
-  const isTupple = (value as unknown[]).length !== 0 && new Set(value as unknown[]).size !== 1;
+  const isTupple = (value as unknown[]).length !== 0 && new Set((value as unknown[]).map((el) => typeof el)).size !== 1;
   let types: string
   if (isTupple) {
     types = JSON
@@ -12,7 +12,7 @@ export function convertArray(key: string, value: unknown): string {
       .replaceAll('"', '')
       .replaceAll(',', ', ')
   } else {
-    types = `${typeof (value as unknown[])[0]}[]`
+    types = `${typeof ((value as unknown[])[0])}[]`
   }
   return `  @Expose({ name: '${key}' })
   public ${convertedKey}: ${types}`
